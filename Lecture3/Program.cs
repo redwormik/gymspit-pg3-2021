6a26202865e8cfdb59bbcbe841ac4e3f9278128a
redwormik <redwormik@gmail.com>
2021-09-23 09:36:26 +0200

    Use i += 1 instead of i++ in Lecture3

--- Lecture3/Program.cs
+++ Lecture3/Program.cs
@@ -64,7 +64,7 @@ namespace Lecture3
 
 			// continue přeruší iteraci cyklu
 			int sum = 0;
-			for (int i = 1; i <= 20; i++) {
+			for (int i = 1; i <= 20; i += 1) {
 				if (i % 3 == 0) {
 					Console.WriteLine("FIZZ!");
 					continue;
