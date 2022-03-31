850fb75e57ff6606c04dfd051a2f94455a325e4a
redwormik <redwormik@gmail.com>
2022-03-31 08:20:54 +0200

    Add Lecture 25

---
+++ Lecture25/Lecture25_1/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lecture25
+{
+	class MyNode<T>
+	{
+		public T value;
+
+		public MyNode<T> next = null;
+
+
+		public MyNode(T value, MyNode<T> next = null)
+		{
+			this.value = value;
+			this.next = next;
+		}
+	}
+
+
+	class MyList<T>
+	{
+		private MyNode<T> head = null;
+
+
+		public bool Empty
+		{
+			get { return head == null; }
+		}
+
+
+		public T First
+		{
+			get { return head.value; }
+		}
+
+
+		public bool Exists(T value)
+		{
+			MyNode<T> node = head;
+
+			while (node != null) {
+				if (ValueEquals(node.value, value)) {
+					return true;
+				}
+
+				node = node.next;
+			}
+
+			return false;
+		}
+
+
+		public IEnumerable<T> Items()
+		{
+			MyNode<T> node = head;
+
+			while (node != null) {
+				yield return node.value;
+				node = node.next;
+			}
+		}
+
+
+		public void InsertFirst(T value)
+		{
+			head = new MyNode<T>(value, head);
+		}
+
+
+		public void RemoveFirst()
+		{
+			head = head.next;
+		}
+
+
+		private bool ValueEquals(T left, T right)
+		{
+			return left == null ?
+				right == null :
+				left.Equals(right);
+		}
+	}
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<string> playlist = new MyList<string>();
+
+			Console.WriteLine(playlist.Empty);
+			// Console.WriteLine(playlist.First);
+
+			playlist.InsertFirst("How much is the fish?");
+			playlist.InsertFirst("Gangnam Style");
+			playlist.InsertFirst("Despacito");
+
+			Console.WriteLine(playlist.Empty);
+			Console.WriteLine(playlist.First);
+
+			Console.WriteLine(playlist.Exists("How much is the fish?"));
+			Console.WriteLine(playlist.Exists("Stánky"));
+
+			foreach (string song in playlist.Items())
+			{
+				Console.WriteLine(song);
+			}
+
+			playlist.RemoveFirst();
+
+			while (!playlist.Empty)
+			{
+				Console.WriteLine(playlist.First);
+				playlist.RemoveFirst();
+			}
+
+			Console.WriteLine(playlist.Empty);
+			// Console.WriteLine(playlist.First);
+
+			Console.WriteLine("Press any key to quit...");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Lecture25/Lecure25_2/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lecture25
+{
+	class MyNode<T>
+	{
+		public T value;
+
+		public MyNode<T> next = null;
+
+
+		public MyNode(T value, MyNode<T> next = null)
+		{
+			this.value = value;
+			this.next = next;
+		}
+	}
+
+
+	class MyList<T>
+	{
+		private MyNode<T> head = null;
+		private MyNode<T> tail = null;
+
+
+		public bool Empty
+		{
+			get { return head == null; }
+		}
+
+
+		public T First
+		{
+			get { return head.value; }
+		}
+
+
+		public T Last
+		{
+			get { return tail.value; }
+		}
+
+
+		public bool Exists(T value)
+		{
+			MyNode<T> node = head;
+
+			while (node != null)
+			{
+				if (ValueEquals(node.value, value)) {
+					return true;
+				}
+
+				node = node.next;
+			}
+
+			return false;
+		}
+
+
+		public IEnumerable<T> Items()
+		{
+			MyNode<T> node = head;
+			while (node != null)
+			{
+				yield return node.value;
+				node = node.next;
+			}
+		}
+
+
+		public void InsertFirst(T value)
+		{
+			head = new MyNode<T>(value, head);
+
+			if (tail == null) {
+				tail = head;
+			}
+		}
+
+
+		public void InsertLast(T value)
+		{
+			MyNode <T> node = new MyNode<T>(value);
+
+			if (tail != null)
+			{
+				tail.next = node;
+			}
+
+			tail = node;
+
+			if (head == null)
+			{
+				head = tail;
+			}
+		}
+
+
+		public void RemoveFirst()
+		{
+			head = head.next;
+		}
+
+
+		private bool ValueEquals(T left, T right)
+		{
+			return left == null ?
+				right == null :
+				left.Equals(right);
+		}
+	}
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<int> numbers = new MyList<int>();
+
+			numbers.InsertLast(1);
+			numbers.InsertLast(2);
+			numbers.InsertLast(3);
+
+			foreach (int number in numbers.Items())
+			{
+				Console.WriteLine(number);
+			}
+
+			numbers.InsertFirst(0);
+
+			Console.WriteLine(numbers.Exists(2));
+			Console.WriteLine(numbers.Exists(4));
+
+			foreach (int number in numbers.Items())
+			{
+				Console.WriteLine(number);
+			}
+
+			numbers.RemoveFirst();
+
+			while (!numbers.Empty)
+			{
+				Console.WriteLine(numbers.First);
+				numbers.RemoveFirst();
+			}
+
+			Console.WriteLine(numbers.Empty);
+			// Console.WriteLine(numbers.First);
+
+			Console.WriteLine("Press any key to quit...");
+			Console.ReadKey();
+		}
+	}
+}
